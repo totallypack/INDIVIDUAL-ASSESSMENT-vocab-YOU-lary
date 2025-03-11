e237e880ff69fa3000530b36c2c82cc523fdd3ef
Chris <christopherpack17@gmail.com>
2025-03-10 21:06:28 -0500

    uid and a touch of styles

--- events/navEvents.js
+++ events/navEvents.js
@@ -1,17 +1,32 @@
-import { getWord, learnedWord } from '../api/wordData';
-import { showWord } from '../pages/words';
+import { getWord } from '../api/wordData';
+import { showWord, emptyWords } from '../pages/words';
 import { signOut } from '../utils/auth';
 
-const navigationEvents = () => {
+const navigationEvents = (user) => {
   document.querySelector('#logout-button')
     .addEventListener('click', signOut);
 
   document.querySelector('#all-learned').addEventListener('click', () => {
-    learnedWord().then(showWord);
+    // Get all words for the user
+    getWord(user.uid).then((words) => {
+      // Filter to only show words where learned === true
+      const learnedWords = words.filter((item) => item.learned === true);
+      // Check if we have any learned words
+      if (learnedWords.length) {
+        // Call showWord with just the filtered words
+        showWord(learnedWords);
+      } else {
+        // If no learned words, show empty state
+        emptyWords();
+      }
+    });
   });
 
-  document.querySelector('#all-words').addEventListener('click', () => {
-    getWord().then(showWord);
+  document.querySelector('#all-words').addEventListener('click', (e) => {
+    if (e.target.id.includes('all-words')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      getWord(user.uid).then((word) => showWord(word.filter((item) => item.category_id === firebaseKey)));
+    }
   });
 
   // STRETCH: SEARCH
